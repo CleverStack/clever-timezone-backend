10e97a4bf893e92d59ac80141e369754f76ac250
Pedro Sampaio <pedro@clevertech.biz>
2014-07-24 21:33:56 +0100

    Update controller and service
    list
    findById
    findByName
    findByOffset

--- controllers/TimezoneController.js
+++ controllers/TimezoneController.js
@@ -2,12 +2,56 @@
  * @doc module
  * @name timezoneModule.controllers:TimezoneController
  */
-module.exports = function( TimezoneService ) {
-    return (require('classes').Controller).extend(
-    {
-        service: TimezoneService
-    },
-    {
-        
-    });
-}
+module.exports = function ( TimezoneService ) {
+    return (require( 'classes' ).Controller).extend(
+        {
+            service: TimezoneService
+        },
+        /* @Prototype */
+        {
+
+            listAction: function () {
+
+                return TimezoneService.list()
+                    .then( this.proxy( 'handleServiceMessage' ) )
+                    .fail( this.proxy( 'handleException' ) );
+            },
+
+            postAction: function () {
+                var action
+                    , data = this.req.body;
+
+                if ( !!data.id ) {
+                    action = TimezoneService.findById( data.id );
+                }
+                else if ( !!data.name ) {
+                    action = TimezoneService.findByName( data.name );
+                }
+                else if ( !!data.offset ) {
+                    action = TimezoneService.findByOffset( data.offset );
+                }
+                else {
+                    return this.send( 'Insufficient data', 400 );
+                }
+
+                return action
+                    .then( this.proxy( 'handleServiceMessage' ) )
+                    .fail( this.proxy( 'handleException' ) );
+            },
+
+            handleServiceMessage: function ( obj ) {
+                if ( !!obj && obj.statuscode ) {
+                    return this.send( obj.message, obj.statuscode );
+                } else {
+                    return this.send( obj, 200 );
+                }
+            },
+
+            handleException: function ( exception ) {
+                if ( typeof exception !== "object" || !exception.stack ) {
+                    return this.send( 500, exception );
+                }
+                this._super( exception );
+            }
+        } );
+};
--- services/TimezoneService.js
+++ services/TimezoneService.js
@@ -1,13 +1,124 @@
-var Q = require('q')
-  , TimezoneService;
+var BaseService = require( 'services' ).BaseService
+    , TimezoneService = null
+    , Q = require( 'q' )
+    , _ = require( 'lodash' );
 
-module.exports = function( sequelize, ORMTimezoneModel ) {
-    TimezoneService = require( 'services' ).BaseService.extend({});
+var errMs = { statuscode: 400, message: "Insufficient data" };
 
-    if ( !TimezoneService.instance ) {
-        TimezoneService.instance = new TimezoneService( sequelize );
-        TimezoneService.Model = ORMTimezoneModel;
+var normalize = function ( data ) {
+    var result;
+    var norm = function ( obj ) {
+        return {
+            id: obj._id || obj.id,
+            name: obj.name,
+            offset: obj.offset
+        }
+    };
+
+    if ( _.isArray( data ) ) {
+        result = [];
+        _.forEach( data, function ( obj ) {
+            result.push ( norm ( obj ) );
+        } );
+        result = _.sortBy( result, 'id' );
+    } else if ( _.isObject( data ) ) {
+        result = norm ( data );
+    } else {
+        result = data;
     }
-    
+
+    return result;
+};
+
+module.exports = function ( sequelize, ORMTimezoneModel ) {
+    if ( TimezoneService && TimezoneService.instance ) {
+        return TimezoneService.instance;
+    }
+
+    TimezoneService = require( 'services' ).BaseService.extend( {
+
+        findById: function ( id ) {
+            var deferred = Q.defer()
+                , query = config.mongo ? { _id: id } : { where: { id: id } };
+
+            this.find( query )
+                .then( function ( result ) {
+                    if ( !result ) {
+                        return deferred.resolve( null );
+                    }
+
+                    deferred.resolve( normalize( result[0] ) );
+                } )
+                .fail( deferred.reject );
+
+            return deferred.promise;
+        },
+
+        findByName: function ( name ) {
+            var self = this
+                , deferred = Q.defer()
+                , query = config.mongo ? { name: name } : { where: { name: name } };
+
+            self.find( query )
+                .then( function ( result ) {
+                    if ( !result ) {
+                        return deferred.resolve( null );
+                    }
+
+                    deferred.resolve( normalize( result [ 0 ] ) );
+                } )
+                .fail( deferred.reject );
+
+            return deferred.promise;
+        },
+
+        findByOffset: function ( offset ) {
+            var deferred = Q.defer();
+
+            if ( !arguments.length ) {
+                deferred.resolve( errMs );
+                return deferred.promise;
+            }
+
+            var obj = {
+                offset: offset
+            };
+
+            var query = config.mongo ? obj : { where: obj };
+
+            this.find( query )
+                .then( function ( result ) {
+                    if ( !result ) {
+                        return deferred.reject( null );
+                    }
+
+                    deferred.resolve( normalize( result [ 0 ] ) );
+                } )
+                .fail( deferred.reject );
+
+            return deferred.promise;
+        },
+
+        list: function ( ) {
+            var deferred = Q.defer();
+
+            this.findAll( )
+                .then( function ( result ) {
+                    if ( !result ) {
+                        return deferred.reject( null );
+                    }
+
+                    deferred.resolve( normalize( result ) );
+                } )
+                .fail( deferred.reject );
+
+            return deferred.promise;
+        }
+
+    } );
+
+    TimezoneService.instance = new TimezoneService( sequelize );
+    TimezoneService.Model = ORMTimezoneModel;
+
     return TimezoneService.instance;
 };
